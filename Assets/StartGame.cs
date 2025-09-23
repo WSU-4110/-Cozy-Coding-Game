feda6534f13bf7f2d46a1250a2e5ffd280234653
Melina Buzgan <melina8b@gmail.com>
2025-09-22 21:57:40 -0400

    Initial commit - add Unity project

---
+++ Assets/StartGame.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void StartTheGame()
+    {
+        // Loads the *next* scene in Build Settings order
+        SceneManager.LoadScene("Game Scene1");
+    }
+}
+
