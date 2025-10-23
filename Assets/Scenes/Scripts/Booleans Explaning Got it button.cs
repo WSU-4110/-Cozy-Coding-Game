e69ff6c11f94b30828fadfb90ecc74c769ade8e5
FaayzK <faayz12khan@gmail.com>
2025-10-23 00:51:43 -0400

    Added BooleanOperatorsQ1 scripts and new scenes

---
+++ Assets/Scenes/Scripts/Booleans Explaning Got it button.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadMiniGame()
+    {
+        SceneManager.LoadScene("Booleans and Operators Mini Game");
+    }
+}
+
