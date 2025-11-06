22beae1bb505fbb0c85fbe6c5b87f735fcb8a2c6
Zaradasht Barakat <zaradashtbarakat76@gmail.com>
2025-11-05 23:24:21 -0500

    Added tutorial page for List level part 2with a functioning button to the list level part 2

---
+++ Assets/Scenes/Scripts/ListlevelP2Start.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ListlevelP2Start : MonoBehaviour
+{
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("ListLevelPart2");
+    }
+}
