2ff57ab0f8815032e34c9ff850cfdc3f220802a3
Tahreem Khanzada <tahreemkhanzada@gmail.com>
2025-11-13 00:24:15 -0500

    Casting level

---
+++ Assets/Scenes/Scripts/CastingQuestionData.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System;
+
+/// <summary>
+
+/// </summary>
+[Serializable]
+public class CastingQuestionData
+{
+    public string QuestionText;
+
+    public string[] AnswerChoices;
+
+    public string CorrectAnswer;
+}
